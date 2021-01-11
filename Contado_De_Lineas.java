4ae7f1d7e6dc4e2b6341a85866aa681e31c46772
Daniel_Perez_99 <daperez@titaniumsolutions.com.mx>
2021-01-11 12:59:59 -0600

    Importe las librerias necesarias y cree las variables globales

--- Contado_De_Lineas.java
+++ Contado_De_Lineas.java
@@ -1,4 +1,14 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.StringTokenizer;
+
+
 public class Contador_De_Lineas{
+
+    static int palabrasTotales;
+    static int lineasTotales;
+
     public static void main(String[] args) {
 
         
