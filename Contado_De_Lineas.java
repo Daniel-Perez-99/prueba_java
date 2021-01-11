4ec5654309972ac92b7d88ae58a54a02ad25c97c
Daniel_Perez_99 <daperez@titaniumsolutions.com.mx>
2021-01-11 13:11:07 -0600

    Cree el metodo contar_Lineas

--- Contado_De_Lineas.java
+++ Contado_De_Lineas.java
@@ -12,7 +12,25 @@ public class Contador_De_Lineas{
     public static void main(String[] args) {
 
         //Creacion del objeto archivo para estar leyendo el archivo
-        File Archivo = new File(“/Users/titaniumac1/documents/archivos_java/ejercicio/prueba_java/Contado_De_Lineas.java”)
+        File archivo = new File(“/Users/titaniumac1/documents/archivos_java/ejercicio/prueba_java/Contado_De_Lineas.java”)
+
+	//Llamar al método que me permitirá contar las lineas
+	contar_Lineas(archivo);
         
     }
+
+    public static void contar_Lineas(File archivo){
+    
+    try{
+    
+        if()
+    
+    } catch(Exception e){
+
+        System.out.println(e.getMessage());
+
+    }
+    
+    }
+
 }
